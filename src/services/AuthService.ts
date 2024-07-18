e4041464348ef13e9a9de6346add6709d5fd5569
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-07-18 10:47:54 +0200

    chore: Add Bootstrap CSS and JS dependencies, update AuthView template

---
+++ src/services/AuthService.ts
@@ -0,0 +1,55 @@
+import { Ref, ref } from "vue";
+
+class AuthService {
+
+  private jwt: Ref<string>;
+  private error: Ref<string>;
+
+  constructor() {
+    this.jwt = ref("");
+    this.error = ref("");
+  }
+
+  getJwt(): Ref<string> {
+    return this.jwt;
+  }
+
+  getError(): Ref<string> {
+    return this.error;
+  }
+
+  async login(email:string, password:string): Promise<boolean> {
+    try {
+      const res = await fetch(
+        'https://directus-production-ad24.up.railway.app/auth/login',
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        }
+      )
+
+      const response = await res.json();
+
+      if ("errors" in response) {
+        this.error.value = "Login failed";
+        return false;
+      }
+
+      this.jwt.value = response.data.access_token;
+      return true;
+
+    } catch (error) {
+      this.error.value = "Login failed";
+      return false;
+    }
+  }
+}
+
+export default AuthService;
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        <h1>Auth View</h1>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    v-model="email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+            </div>
+            <button type="submit" class="btn btn-primary mt-2" @click.prevent="authUser">Submit</button>
+        </form>
     </div>
 </template>
+
+<script lang="ts" setup>
+
+import { ref } from 'vue';
+import AuthService from '@/services/AuthService';
+
+let email = ref('');
+let password = ref('');
+
+const authUser = async () => {
+    const auth = new AuthService();
+    const success = await auth.login(email.value, password.value);
+    if(success) {
+        alert('Login Success!');
+    } else {
+        alert('Login Failed!');
+    }
+}
+
+</script>
