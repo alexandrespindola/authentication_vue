ea35c17e0952dc9bca969f620dbf2a033d608ce2
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-07-20 11:38:00 +0200

    chore: Update navigation links and form layout
    ```

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/auth">Login / Register</router-link> |
-    <router-link to="/firebase">Firebase Login</router-link>
+    <router-link to="/auth">Backend</router-link> |
+    <router-link to="/firebase">Firebase</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/FirebaseView.vue
+++ src/views/FirebaseView.vue
@@ -1,8 +1,37 @@
 <template>
-    <div>
-      <h1>Firebase</h1>  
-    </div>
+  <div class="container">
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+      </div>
+      <button type="submit" class="btn btn-primary mt-2" @click.prevent="authUser">Submit</button>
+    </form>
+  </div>
 </template>
 
-<script>
+<script lang="ts" setup>
+
+import { Ref, ref } from 'vue'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+let email: Ref<string> = ref('');
+let password: Ref<string> = ref('');
+
+const authUser = () => {
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => {
+      // Signed in
+      alert('User signed in!');
+    })
+    .catch((error) => {
+      alert('Error: ' + error.message);
+    });
+}
+
 </script>
