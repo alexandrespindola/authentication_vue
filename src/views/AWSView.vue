1c8cf661d9bf41a40b11ddb5dab454f15b6945ed
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-07-22 10:25:41 +0200

    chore: Add AWS authentication to AWSView template

--- src/views/AWSView.vue
+++ src/views/AWSView.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
         <h1>AWS</h1>
+        <authenticator>
+            <template v-slot="{user, signOut}">
+                <h1>Hello {{ user.username }}</h1>
+                <button @click="signOut">Sign out</button>  
+            </template>
+        </authenticator>
     </div>
 </template>
 
 <script lang="ts" setup>
+
+import { Authenticator } from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css';
+
+
 </script>
